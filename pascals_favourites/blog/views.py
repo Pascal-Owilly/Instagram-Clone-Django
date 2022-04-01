2a21e02b236720c39b99db3ea153fa119a795497
Owilly <pasaclouma54@gmail.com>
2022-04-01 13:24:42 +0300

    only allow the owners of te post to update them

--- pascals_favourites/blog/views.py
+++ pascals_favourites/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView
+    CreateView,
+    UpdateView
 )
 from .models import Post
 
@@ -22,6 +23,7 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
      
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -29,6 +31,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(UserPassesTestMixin,LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
